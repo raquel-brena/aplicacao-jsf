12563021c5f044d0c6d5cd244544468d991c412e
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2025-02-10 03:28:07 -0300

    chore: add security filter

---
+++ src/main/java/com/rb/esig/infra/security/SecurityFilter.java
@@ -0,0 +1,35 @@
+package com.rb.esig.infra.security;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class SecurityFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpSession session = req.getSession(false);
+
+        boolean loggedIn = (session != null && session.getAttribute("usuarioLogado") != null);
+        boolean loginRequest = req.getRequestURI().endsWith("login.xhtml");
+
+        if (!loggedIn && !loginRequest) {
+            res.sendRedirect(req.getContextPath() + "/login.xhtml");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
