fe0b986ae02b29f95094928f868a14c6c89c3357
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2025-02-11 01:57:43 -0300

    feat: add cargo bean, repository e service

--- src/main/java/com/rb/esig/repositories/CargoRepository.java
+++ src/main/java/com/rb/esig/repositories/CargoRepository.java
@@ -5,11 +5,14 @@ import com.rb.esig.infra.utils.JpaUtil;
 import com.rb.esig.shared.exceptions.BadRequestException;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import java.util.List;
+import java.util.Optional;
 
 public class CargoRepository {
 
+    @PersistenceContext
     private EntityManager entityManager;
 
     public CargoRepository() {
@@ -17,19 +20,37 @@ public class CargoRepository {
     }
 
     public List<Cargo> findAll() {
-        return entityManager.createQuery("SELECT p FROM Cargo p", Cargo.class)
-                .getResultList();
+        try {
+            return entityManager.createQuery("SELECT p FROM Cargo p", Cargo.class)
+                    .getResultList();
+        } catch (NumberFormatException e) {
+            throw new BadRequestException("Não foi possível buscar os cargos");
+        }
     }
 
-    public Cargo findById(Long id) {
+    public Optional<Cargo> findById(Long id) {
         try {
             TypedQuery<Cargo> query = entityManager.createQuery(
                     "SELECT c FROM Cargo c WHERE c.id = :id", Cargo.class);
             query.setParameter("id", id);
 
-            return query.getSingleResult();
+            return Optional.of(query.getSingleResult());
         } catch (NumberFormatException e) {
             throw new BadRequestException("ID inválido: " + id);
+        }
+    }
+
+    public Optional<Cargo> findByName(String name) {
+        try {
+
+            TypedQuery<Cargo> query = entityManager.createQuery(
+                    "SELECT c FROM Cargo c WHERE c.nome = :name", Cargo.class);
+            query.setParameter("name", name);
+
+            System.out.println(query.getSingleResult());
+            return Optional.of(query.getSingleResult());
+        } catch (NumberFormatException e) {
+            throw new BadRequestException("Houve um erro ao buscar o cargo: " + name);
         }
     }
 
--- src/main/java/com/rb/esig/services/CargoService.java
+++ src/main/java/com/rb/esig/services/CargoService.java
@@ -2,10 +2,12 @@ package com.rb.esig.services;
 
 import com.rb.esig.domain.Cargo;
 import com.rb.esig.repositories.CargoRepository;
+import com.rb.esig.shared.exceptions.NotFoundException;
 
 import javax.enterprise.context.Dependent;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 @Dependent
 public class CargoService implements Serializable {
@@ -18,7 +20,19 @@ public class CargoService implements Serializable {
     }
 
     public Cargo findById(Long id) {
-        return repository.findById(id);
+        Optional<Cargo> cargo = repository.findById(id);
+
+        if (cargo.isEmpty()) throw new NotFoundException("Cargo não encontrado: " + id);
+
+        return cargo.get();
+    }
+
+    public Cargo findByName(String name) {
+        Optional<Cargo> cargo = repository.findByName(name);
+
+        if (cargo.isEmpty()) throw new NotFoundException("Cargo não encontrado: " + name);
+
+        return cargo.get();
     }
 
     public List<Cargo> findAll() {
